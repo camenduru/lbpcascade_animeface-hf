88236cc96b0ab44defd69c60daff70bf0b3df842
hysts <hysts@users.noreply.huggingface.co>
2022-01-24 17:49:20 +0900

    Add files

---
+++ app.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+
+from __future__ import annotations
+
+import argparse
+import functools
+import os
+import pathlib
+import tarfile
+import urllib
+
+import cv2
+import gradio as gr
+import huggingface_hub
+import numpy as np
+
+TOKEN = os.environ['TOKEN']
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--theme', type=str)
+    parser.add_argument('--live', action='store_true')
+    parser.add_argument('--share', action='store_true')
+    parser.add_argument('--port', type=int)
+    parser.add_argument('--disable-queue',
+                        dest='enable_queue',
+                        action='store_false')
+    parser.add_argument('--allow-flagging', type=str, default='never')
+    parser.add_argument('--allow-screenshot', action='store_true')
+    return parser.parse_args()
+
+
+def load_sample_image_paths() -> list[pathlib.Path]:
+    image_dir = pathlib.Path('images')
+    if not image_dir.exists():
+        dataset_repo = 'hysts/sample-images-TADNE'
+        path = huggingface_hub.hf_hub_download(dataset_repo,
+                                               'images.tar.gz',
+                                               repo_type='dataset',
+                                               use_auth_token=TOKEN)
+        with tarfile.open(path) as f:
+            f.extractall()
+    return sorted(image_dir.glob('*'))
+
+
+def load_model() -> cv2.CascadeClassifier:
+    url = 'https://raw.githubusercontent.com/nagadomi/lbpcascade_animeface/master/lbpcascade_animeface.xml'
+    path = pathlib.Path('lbpcascade_animeface.xml')
+    if not path.exists():
+        urllib.request.urlretrieve(url, path.as_posix())
+    return cv2.CascadeClassifier(path.as_posix())
+
+
+def detect(image, detector: cv2.CascadeClassifier) -> np.ndarray:
+    image = cv2.imread(image.name)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    preds = detector.detectMultiScale(gray,
+                                      scaleFactor=1.1,
+                                      minNeighbors=5,
+                                      minSize=(24, 24))
+
+    res = image.copy()
+    for x, y, w, h in preds:
+        cv2.rectangle(res, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    res = cv2.cvtColor(res, cv2.COLOR_BGR2RGB)
+    return res
+
+
+def main():
+    gr.close_all()
+
+    args = parse_args()
+
+    image_paths = load_sample_image_paths()
+    examples = [[path.as_posix()] for path in image_paths]
+
+    detector = load_model()
+    func = functools.partial(detect, detector=detector)
+    func = functools.update_wrapper(func, detect)
+
+    repo_url = 'https://github.com/nagadomi/lbpcascade_animeface'
+    title = 'nagadomi/lbpcascade_animeface'
+    description = f'A demo for {repo_url}'
+    article = None
+
+    gr.Interface(
+        func,
+        gr.inputs.Image(type='file', label='Input'),
+        gr.outputs.Image(label='Output'),
+        theme=args.theme,
+        title=title,
+        description=description,
+        article=article,
+        examples=examples,
+        allow_screenshot=args.allow_screenshot,
+        allow_flagging=args.allow_flagging,
+        live=args.live,
+    ).launch(
+        enable_queue=args.enable_queue,
+        server_port=args.port,
+        share=args.share,
+    )
+
+
+if __name__ == '__main__':
+    main()
